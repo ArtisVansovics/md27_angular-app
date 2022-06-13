0528d752c5876cbe0c019031bea76304858334af
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 23:07:22 +0300

    Created basic project structure

---
+++ src/app/components/jokes/jokes-card/jokes-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jokes-card',
+  templateUrl: './jokes-card.component.html',
+  styleUrls: ['./jokes-card.component.scss']
+})
+export class JokesCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
