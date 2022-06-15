bdd0efa963b7076da4086f77cc5bb140333e371c
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-15 18:43:39 +0300

    Created custom button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input() buttonTitle: String | undefined;
+
+  @Input() buttonColor: String | undefined;
+
+  @Input() isDisabled: Boolean | undefined;
+
+  @Output() buttonClickEvent = new EventEmitter();
+
+  onClick(): void {
+    this.buttonClickEvent.emit();
+  }
+}
